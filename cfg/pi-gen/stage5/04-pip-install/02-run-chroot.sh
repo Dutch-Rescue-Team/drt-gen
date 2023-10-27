113cff6b11dc50ba9adf204dbcf29d68092a94a3
Iwan Nijs - de Boer <iwan@nicethoughts.eu>
2023-10-27 08:14:48 +0100

    Fixed pip install pupil-apriltags==1.0.4.

--- cfg/pi-gen/stage5/04-pip-install/02-run-chroot.sh
+++ cfg/pi-gen/stage5/04-pip-install/02-run-chroot.sh
@@ -1,4 +1,13 @@
 #!/bin/bash -e
 
 pip install python_json_config
+
 # pip install pupil-apriltags==1.0.4
+# Installing pupil-apriltags==1.0.4 using pip fails. So, a workaround.
+WHEEL_FILE=pupil_apriltags-1.0.4-cp39-cp39-linux_armv7l.whl
+SOURCE=https://www.piwheels.org/simple/pupil-apriltags/$WHEEL_FILE
+TARGET=/tmp/$WHEEL_FILE
+wget --quiet --tries=2 -O $TARGET $SOURCE
+unzip -d /usr/local/lib/python3.9/dist-packages $TARGET
+echo APRILTAGS unzipped
+rm $TARGET
